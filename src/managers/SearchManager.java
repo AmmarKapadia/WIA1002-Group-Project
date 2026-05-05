31f3c3165080c0de37e0fae2232d30f8af52faa4
XU ZIMAO <24216617@siswa.um.edu.my>
2026-05-05 13:58:09 +0800

    BST Serch System

--- src/managers/SearchManager.java
+++ src/managers/SearchManager.java
@@ -1,5 +1,24 @@
 package managers;
 
-public class SearchManager {
+import datastructures.CustomBST;
+import models.Vehicle;
 
+/**
+ * Implement BST Serch
+ * @author XU ZIMAO
+ */
+public class SearchManager {
+    private CustomBST bst;
+
+    public SearchManager() {
+        this.bst = new CustomBST();
+    }
+
+    public void addVehicleRecord(Vehicle vehicle) {
+        bst.insert(vehicle);
+    }
+
+    public Vehicle findVehicle(String licensePlate) {
+        return bst.search(licensePlate);
+    }
 }
