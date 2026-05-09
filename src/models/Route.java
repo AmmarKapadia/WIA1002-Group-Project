5bd34be7d3fc2eebfcad0dc872ba6ecd1418dd4e
Ammar Kapadia <24217261@siswa.um.edu.my>
2026-05-09 16:52:49 +0800

    Make Route fields private and final
    Changed path and totalDistance fields to be private final for immutability.

--- src/models/Route.java
+++ src/models/Route.java
@@ -7,16 +7,25 @@ package models;
 import java.util.ArrayList;
 
 /**
+ * Immutable result of a Dijkstra path search: the ordered list of vertex
+ * names from source to destination, plus the total path weight.
  *
  * @author Yim Zi Hao
  */
 public class Route {
-
-    public ArrayList<String> path;
-    public int totalDistance;
+    private final ArrayList<String> path;
+    private final int totalDistance;
 
     public Route(ArrayList<String> path, int totalDistance) {
         this.path = path;
         this.totalDistance = totalDistance;
+    }
+
+    public ArrayList<String> getPath() {
+        return path;
+    }
+
+    public int getTotalDistance() {
+        return totalDistance;
     }
 }
