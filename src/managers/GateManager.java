a195ff742a31ef067431a3103beb534563edfe28
Ammar Kapadia <24217261@siswa.um.edu.my>
2026-05-19 01:56:18 +0800

    Remove unused import from GateManager.java
    Removed unnecessary import statement for HistoryManager.Action.

--- src/managers/GateManager.java
+++ src/managers/GateManager.java
@@ -5,8 +5,6 @@ import models.Vehicle;
 import models.ParkingSlot;
 import java.util.EmptyStackException;
 
-import managers.HistoryManager.Action;;
-
 /**
  * Coordinates the gate flow: arrival queue, slot assignment,
  * exit cleanup, history management and undo.
